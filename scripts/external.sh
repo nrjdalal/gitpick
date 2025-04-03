b119bfc5d4d1c28160793951972bb2a16ace7040
Neeraj Dalal <admin@nrjdalal.com>
2025-04-03 16:48:49 +0530

    chore: tweaks > external.sh
    scripts/external.sh

--- scripts/external.sh
+++ scripts/external.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# bun gitpick@latest https://github.com/sindresorhus/nano-spawn/tree/d3d724082c6bc679079e150b25b5b8242a2af2c4/source external/nano-spawn -f
-bunx gitpick@latest https://github.com/sindresorhus/nano-spawn/tree/main/source external/nano-spawn -f
+bunx gitpick@latest https://github.com/sindresorhus/nano-spawn/tree/d3d724082c6bc679079e150b25b5b8242a2af2c4/source external/nano-spawn -f
 bunx prettier@latest --write --ignore-unknown "external"
